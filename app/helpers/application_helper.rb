b8bfe4dd006ff3291998c8a9d1cf48c4c6d91cd5
T Riboulet <riboulet@gmail.com>
2010-03-17 12:45:11 +0100

    adding current user method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,12 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def current_user_session
+		return @current_user_session if defined?(@current_user_session)
+		@current_user_session = UserSession.find
+	end
+
+	def current_user
+		return @current_user if defined?(@current_user)
+		@current_user = current_user_session && current_user_session.record
+	end
 end
