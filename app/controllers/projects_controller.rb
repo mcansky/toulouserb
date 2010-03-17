1e3a91d6b7974f27a962725ead67ad9de7c80d71
T Riboulet <riboulet@gmail.com>
2010-03-17 16:26:12 +0100

    pagination and tag lovin' for project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,8 @@
 class ProjectsController < ApplicationController
   before_filter :require_login, :except => ["index", "show"]
   def index
-    @projects = Project.all
+    @projects = Project.find_tagged_with(params[:tag]).reverse if params[:tag]
+    @projects = Project.all.reverse.paginate :page => params[:page] unless @posts
   end
   
   def show
@@ -14,6 +15,9 @@ class ProjectsController < ApplicationController
   
   def create
     @project = Project.new(params[:project])
+    params[:project][:tag_list].split(',').each do |tag|
+      @project.tag_list.add(tag.gsub(' ', ''))
+    end
     if @project.save
       flash[:notice] = "Successfully created project."
       redirect_to @project
@@ -28,6 +32,12 @@ class ProjectsController < ApplicationController
   
   def update
     @project = Project.find(params[:id])
+    if params[:project][:tag_list]
+      @project.tag_list.each { |t| @post.tag_list.remove(t) }
+    end
+    params[:project][:tag_list].split(',').each do |tag|
+      @project.tag_list.add(tag.gsub(' ', ''))
+    end
     if @project.update_attributes(params[:project])
       flash[:notice] = "Successfully updated project."
       redirect_to @project
