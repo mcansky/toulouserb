280f4dd8e04415da1edd3566aaa5267993bf6159
T Riboulet <riboulet@gmail.com>
2010-03-22 20:52:03 +0100

    changing some validations and adding correct test

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,9 @@
 class Post < ActiveRecord::Base
   attr_accessible :title, :content, :published, :doc
-  has_attached_file :doc,
+  has_attached_file :doc, :allow_nil => true,
     :url  => "/posts/doc/:id",
     :path => ":rails_root/public/system/:id/:basename.:extension"
-  validates_attachment_size :doc, :less_than => 5.megabytes
-  validates_attachment_content_type :doc, :content_type => ['application/pdf']
+  validates_attachment_content_type :doc, :content_type => ['application/pdf'], :allow_nil => true
   acts_as_taggable
   acts_as_textiled :content
   cattr_reader :per_page
