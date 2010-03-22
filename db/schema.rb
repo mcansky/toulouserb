0402820d71316c1e47a0b8182a9350536f1cb4f2
T Riboulet <riboulet@gmail.com>
2010-03-22 19:07:17 +0100

    adding post doc attachements

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100318135338) do
+ActiveRecord::Schema.define(:version => 20100322180355) do
 
   create_table "partners", :force => true do |t|
     t.string   "name"
@@ -26,6 +26,10 @@ ActiveRecord::Schema.define(:version => 20100318135338) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "published"
+    t.string   "doc_file_name"
+    t.string   "doc_content_type"
+    t.integer  "doc_file_size"
+    t.datetime "doc_updated_at"
   end
 
   create_table "projects", :force => true do |t|
